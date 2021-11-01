b489f366c930056d2be4444bbd27d203f82ab566
unknown <nolan.beihl@gmail.com>
2021-11-01 13:36:27 -0600

    Created employee model, employee table, employee model, urls, views, and local table in database for use

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    zip_code = models.CharField(max_length=5)
+    work_days = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,13 +1,42 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.apps import apps
-
+from django.urls import reverse
+from .models import Employee
+from datetime import date
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
 
+@login_required
 def index(request):
-    # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    # The following line will get the logged-in user (if there is one) within any view function
+    logged_in_user = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today
+        }
+        return render(request, 'employees/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        zip_from_form = request.POST.get('zip_code')
+        work_days_from_form = request.POST.get('work_days')
+        new_employee = Employee(name=name_from_form, user=logged_in_user,  zip_code=zip_from_form, work_days=work_days_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
