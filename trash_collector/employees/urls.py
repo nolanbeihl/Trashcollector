6c8fdcbcae27b4ccec0022263d5380b69365f8f9
James Corbett <wjamescorbett@gmail.com>
2021-11-03 10:33:43 -0400

    Employee edit profile works properly.

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -8,4 +8,5 @@ app_name = "employees"
 urlpatterns = [
     path('', views.index, name="index"),
     path('new/', views.create, name="create"),
+    path('edit_profile/', views.edit_profile, name="edit_profile"),
 ]
